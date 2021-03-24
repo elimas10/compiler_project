f9c2b3d5f1a66cd71344fd4644e31efb164a51c6
elie <masoumi.elnaz2@gmail.com>
2021-03-24 22:11:13 +0430

    first commit for reading inputs

---
+++ scanner.py
@@ -0,0 +1,10 @@
+
+
+
+program = open('input.txt', 'r').read()
+lexical_errors = open('lexical_errors.txt', 'r').read()
+symbol_table = open('lexical_errors.txt', 'r').read()
+tokens = open('tokens.txt', 'r').read()
+
+
+
